e4527025fc82c530b7756aac9ae30617d2d9733a
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-08-04 22:48:42 +0900

    Baekjoon 1003
    Dynamic Programming

---
+++ DynamicProgramming/fib01DP.cpp
@@ -0,0 +1,24 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+	int n, d;
+	long long v[41] = {0, 1, 1};
+	for(int i = 3; i < 41; i++){
+		v[i] = v[i - 1] + v[i - 2];
+	}
+
+	cin >> n;
+
+	for(int i = 0; i < n ; i++){
+		cin >> d;
+		if(d == 0){
+			cout << 1 << ' ' << 0 << '\n';
+		} else if (d == 1){
+			cout << 0 << ' ' << 1 << '\n';
+		} else {
+			cout << v[d - 1] << ' ' << v[d] << '\n';
+		}
+	}
+	return 0;
+}*/
