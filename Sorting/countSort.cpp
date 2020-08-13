2105a6ce856ae7b113889745fb0269c1cfd26db4
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-08-13 16:29:23 +0900

    Baekjoon 10989
    Sorting

---
+++ Sorting/countSort.cpp
@@ -0,0 +1,20 @@
+/*#include <iostream>
+using namespace std;
+
+int main(){
+	int arrs[10001] = {0};
+	int N;
+	cin >> N;
+	
+	for(int i = 0; i < N; i++){
+		int t;
+		cin >> t;
+		arrs[t]++;
+	}
+	for(int i = 1; i <= 10000; i++){
+		for(int j = 0; j < arrs[i]; j++){
+			cout << i << '\n';
+		}
+	}
+	return 0;
+}*/
