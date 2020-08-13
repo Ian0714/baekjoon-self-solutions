1649d318408af3800947a04877a6e772e234161c
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-08-13 16:30:05 +0900

    Baekjoon 1427
    Sorting

---
+++ Sorting/insideSort.cpp
@@ -0,0 +1,26 @@
+/*#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define A 1000000000
+long long T[9];
+
+int main(){
+	long long N;
+	int count = 0;
+	cin >> N;
+	if(A == N){
+		cout << 1000000000;
+	} else {
+		while(N != 0){
+			T[count] = N % 10;
+			N /= 10;
+			count++;
+		}
+		sort(T, T + count);
+		for(int i = count - 1; i >= 0; i--){
+			cout << T[i];
+		}
+	}
+	return 0;
+}*/
