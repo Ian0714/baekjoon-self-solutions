4531f86c05921add24c6e5cf9a81d25852517dda
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-08-16 22:56:39 +0900

    Baekjoon 1436

---
+++ etc/endMovie.cpp
@@ -0,0 +1,20 @@
+/*#include <iostream>
+#include <string>
+
+using namespace std;
+
+string comp = "666";
+int count = 0, N, start = 665;
+
+int main(){
+	cin >> N;
+	while(N != count){
+		start++;
+		string c = to_string(start);
+		if(c.find(comp) != -1){
+			count++;
+		}
+	}
+	cout << start;
+	return 0;
+}*/
